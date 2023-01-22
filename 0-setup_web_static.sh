161a861396fb5b5a2647279d4da87de3a3e6be3e
Kingsley <dondecency11@gmail.com>
2023-01-22 08:21:57 +0000

    2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #Setup the webserver for deployement
 apt update -y
 apt install nginx -y
@@ -7,5 +7,5 @@ mkdir -p /data/web_static/shared/
 echo "<h1>Test me</h1>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -Rh ubuntu:ubuntu /data/
-sed -i.bak '/^server {$/a\\t location /hbnb_static {\n\t\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
+sed -i '/^server {$/a\\t location /hbnb_static {\n\t\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
 service nginx restart
